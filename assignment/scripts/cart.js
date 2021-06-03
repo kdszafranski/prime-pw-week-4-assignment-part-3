dc49cdb8bb7ea3890d7581aee4539e5544aea458
Kris Szafranski <kris@thoughtshelter.com>
2021-06-03 12:32:41 -0500

    added sayhello example

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,18 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+// function definition
+// expects to be given a string called 'name'
+function sayHello( name ) {
+  // let name = 'Tate';
+
+  // name only exists in here
+  console.log('Hello, ' + name);
+}
+
+// "call" the function, makes it actually run
+sayHello('Tate');
+sayHello('Dane');
+sayHello('Chad');
