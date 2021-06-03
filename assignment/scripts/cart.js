acff602a4e9331d7e9e6c5f23c72350ac1783860
Kris Szafranski <kris@thoughtshelter.com>
2021-06-03 13:19:26 -0500

    end of class

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -16,3 +16,79 @@ function sayHello( name ) {
 sayHello('Tate');
 sayHello('Dane');
 sayHello('Chad');
+
+
+function doubleIt(number) {
+  // return provides output from the function
+  return number * 2;
+}
+
+let answer = doubleIt(3);
+console.log('the answer is: ' + answer);
+console.log(`the answer is ${answer}`);
+
+// versatile
+console.log('the answer is: ', answer);
+
+console.log('the answer is: ', doubleIt(7));
+
+
+
+// CART STUFF
+
+let basket = []; // global
+const MAX_ITEMS = 3; // constant global
+const DAYS_OF_THE_WEEK = 7;
+const PI = 3.1415;///
+
+function addItem( item ) {
+  if( isFull() ) {
+    // already is full
+    console.log('No more room!');
+  } else {
+    // there is room, add the item
+    basket.push(item);
+  }
+
+}
+//
+// if( /* something evaluates to true */) {
+//   // do true
+// } else {
+//   // do false
+// }
+
+function isFull() {
+  // 3
+  if(basket.length < MAX_ITEMS) {
+    // can have more items
+    return false;
+  } else {
+    // yes, basket is full
+    return true;
+  }
+}
+
+// add some items
+addItem('sushi');
+addItem('pizza');
+addItem('ice cream');
+console.log(basket);
+
+
+addItem('watermelon');
+
+// when in doubt, log it out --Chris
+console.log(basket);
+
+
+
+// function expression
+let tripleIt = function(num){
+  return num * 3;
+}
+
+// call setInterval(function () {
+console.log( tripleIt(3) );
+
+console.log( tripleIt );
